0fe599c058d6f2fe80dacce5b581ee3bd4a97116
Jerry Augusto <jerryaugustodev@gmail.com>
2022-04-14 08:38:55 -0400

    Creating 'game' module and refactoring the 'ex_mon' file.

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Player, Game}
+
+  @computer_name "Roshan"
 
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_avg, move_heal, move_rnd)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:bash, :spell_block, :slam)
+    |> Game.start(player)
   end
 end
